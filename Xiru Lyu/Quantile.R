b2ce5f106185b0fa10d3dee7bd6a651938971518
Xiru Lyu (Sheryl) <35626962+xrlyu@users.noreply.github.com>
2019-12-11 23:51:04 -0500

    Update Quantile.R
    Add comments and header to the file.

--- Xiru Lyu/Quantile.R
+++ Xiru Lyu/Quantile.R
@@ -1,11 +1,25 @@
+# STATS 506, FALL 2019
+#
+# Quantile.R
+#
+# This file contains quantile regression results and the quantile regression plot.
+#
+# Author: Xiru Lyu, xlyu@umich.edu
+#
+# Last Updated: Dec 11, 2019
+
+# Load required packages
 library(quantreg)
 library(tidyverse)
 library(ggplot2)
 
+# Import the dataset
 data <- readRDS("./Xiru Lyu/Cleaning/final.rds")
 
+# Set quantiles
 tau <- c(0.05, 0.25, 0.5, .75, 0.9)
 
+# Fit quantile regression with different quantiles
 fit1 <- rq(sqrt(ldl) ~ trig, tau = tau[1], data = data)
 fit2 <- rq(sqrt(ldl) ~ trig, tau = tau[2], data = data)
 fit3 <- rq(sqrt(ldl) ~ trig, tau = tau[3], data = data)
@@ -15,6 +29,7 @@ fit5 <- rq(sqrt(ldl) ~ trig, tau = tau[5], data = data)
 data2 <- data %>% select(ldl, trig) %>%
   mutate(ldl2 = sqrt(ldl))
 
+# Graph the quantile regression plot
 ggplot() + 
   geom_point(data = data2, aes(x = trig, y = ldl2), color = "grey39") +
   geom_abline(aes(intercept = fit1$coefficients[1], slope = fit1$coefficients[2],
